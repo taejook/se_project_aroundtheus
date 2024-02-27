90d8b292df01661f31e21a6dab68bb88cc9d262a
TaeJoo Kim <taejook1107@gmail.com>
2024-02-27 17:09:37 -0500

    added initialCards

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,33 @@
-const yosemite = {
+const initialCards=[
+
+{
     name: "Yosemite Valley",
     url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-}
+},
 
-const louise = {
+{
     name: "Lake Louise",
     url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-}
-
-const bald = {
+},
+{
     name: "Bald Mountains",
     url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-}
+},
 
-const latemar = {
+{
     name: "Latemar",
     url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-}
+},
 
-const vanoise = {
+{
     name: "Vanoise National Park",
     url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-}
+},
 
-const braise = {
+{
     name: "Lago di Braies",
     url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
 }
+]
+
+console.log(initialCards);
