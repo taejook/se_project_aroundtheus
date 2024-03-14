cc353be6c51f238ec79afacc7f4e7454928158c8
TaeJoo Kim <taejook1107@gmail.com>
2024-03-14 17:59:54 -0400

    added functions and elements to the index.js

--- scripts/index.js
+++ scripts/index.js
@@ -29,7 +29,7 @@ const initialCards=[
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
 }
 ]
-
+/*Elements*/
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditCloseBtn = document.querySelector("#profile-edit-close");
@@ -39,7 +39,8 @@ const profileEditTitleInput = document.querySelector('#profile-title-input');
 const profileEditDescriptionInput = document.querySelector('#profile-description-input');
 
 const profileEditForm = profileEditModal.querySelector('.modal__form');
-
+const cardListEl = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card-template').content.firstElementChild;
 /*Functions*/
 function closePopUp(){
     profileEditModal.classList.remove('modal__opened');
@@ -55,6 +56,16 @@ function handleProfileEditSubmit(e){
     profileDescription.textContent = profileEditDescriptionInput.value;
     closePopUp();
 };
+
+function getCardElement(cardData){
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImageEl = cardElement.querySelector('.card__image');
+    const cardTitleEl = cardElement.querySelector('.cards__title');
+    cardImageEl.setAttribute('src', cardData.link);
+    cardImageEl.setAttribute('alt', cardData.name);
+    cardTitleEl.textContent = cardData.name;
+    return cardElement;
+}
 /*Event Listeners*/
 profileEditBtn.addEventListener('click', () =>{
     profileEditTitleInput.value = profileTitle.textContent;
@@ -65,3 +76,8 @@ profileEditBtn.addEventListener('click', () =>{
 profileEditCloseBtn.addEventListener('click',closePopUp);
 
 profileEditForm.addEventListener('submit', handleProfileEditSubmit);
+
+initialCards.forEach((cardData) =>{
+    const cardElement = getCardElement(cardData);
+    cardListEl.prepend(cardElement);
+});
